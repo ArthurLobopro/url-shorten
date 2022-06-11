a2a650e50f506ce618764186900567af34904bce
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2022-06-11 07:43:52 -0300

    chore: add cors in backend

--- server/src/server/index.ts
+++ server/src/server/index.ts
@@ -1,26 +1,28 @@
-import * as path from 'path'
-import express from 'express'
-import { exec } from 'child_process'
-import { router } from './routes'
+import * as path from "path"
+import express from "express"
+import { exec } from "child_process"
+import { router } from "./routes"
+import cors from "cors"
 
 const getPort = () => {
     const defaultPort = 4000
     const args = process.argv
-    const portARG = args.find(arg => arg.includes('--port='))
-    return Number(portARG?.split('=')?.[1]) || defaultPort
+    const portARG = args.find(arg => arg.includes("--port="))
+    return Number(portARG?.split("=")?.[1]) || defaultPort
 }
 
 const server = express()
 const root = process.cwd()
 const port = getPort()
 
+server.use(cors())
 server.use(express.json())
-server.use(express.static(path.resolve(root, '../build')))
+server.use(express.static(path.resolve(root, "../build")))
 server.use(express.urlencoded({ extended: true }))
 server.use(router)
 
 server.listen(port, () => console.log(`Server Online, Port: ${port}`))
 
 if (process.argv.includes("--dev")) {
-    exec(`${process.platform === "darwin" ? 'open' : 'start'} http://localhost:${port}`)
+    exec(`${process.platform === "darwin" ? "open" : "start"} http://localhost:${port}`)
 }
